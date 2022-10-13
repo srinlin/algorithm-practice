518da29b986c91683b896f261f21847dee41753c
Serin <105341794+srinlin@users.noreply.github.com>
2022-10-13 22:49:58 +0900

    [Bronze III] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,7 @@
+a, b, c = map(int, input())
+d, e, f = map(int, input())
+
+print((100*a + 10*b + c)*f)
+print((100*a + 10*b + c )*e)
+print((100*a + 10*b + c) * d )
+print((100*a + 10*b + c)*(100*d + 10*e + f))
